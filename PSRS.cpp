bea0ac322b43da196d9be8c1b8c867e46d3821e9
Prion6 <gsepulveda17@alumnos.utalca.cl>
2020-12-12 18:56:36 -0300

    Update PSRS.cpp

--- PSRS.cpp
+++ PSRS.cpp
@@ -8,15 +8,43 @@
 #include <queue>
 #include <ctime>
 
-const int N = 400;
-const int RANGE = 100000;
+int N = 1000;
 int PROCESSOR = 8;
 
-void Swap(int* a, int* b);
-int Partition (int *array, int low, int high) ;
-void QuickSort(int *array, int low, int high);
 std::vector<int> PSRS(std::vector<int> array, int rank);
 std::vector<int> MergeSort(std::vector<int> nums, int rank);
+
+std::vector<int> RandomVector(int size, int Range)
+{
+    std::vector<int> array(size);
+    std::mt19937 rng;
+    for(int i = 0; i < size; i++)
+    {
+        rng.seed(clock());
+        std::generate(array.begin(),array.end(),[&](){return rng()%Range;});
+    }
+    return array;
+}
+
+std::vector<int> SortedVector(int size)
+{
+    std::vector<int> array(size);
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = i;
+    }
+    return array;
+}
+
+std::vector<int> BackwardsVector(int size)
+{
+    std::vector<int> array(size);
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = size-i;
+    }
+    return array;
+}
 
 
 
@@ -28,85 +56,90 @@ int main(int argc, char *argv[])
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank); // ID of current process
 	MPI_Get_processor_name (name, &namelen); // Hostname of node
 	MPI_Comm_size (MPI_COMM_WORLD, &PROCESSOR); // Number of processes
+
+    N = atoi(argv[1]);
+    int order = atoi(argv[2]);
     
     std::vector<int> array(N);
     std::vector<int> mergeArray(N);
 
-    auto totalTime = 0;
 
-    for(int i = 0; i < 100; i++)
+    if(rank == 0)
     {
-        auto startTime = std::chrono::high_resolution_clock::now();
-
-        if(rank == 0)
+        switch (order)
         {
-            //Initialization of vector
-            std::mt19937 rng;
-            for(int i = 0; i < N; i++)
-            {
-                rng.seed(clock());
-                std::generate(array.begin(),array.end(),[&](){return rng()%RANGE;});
-            }
+            case 1: array = SortedVector(N);
+            break;
+            case 2: array = BackwardsVector(N);
+            break;
+            default: array = RandomVector(N,N*100);
+            break;
         }
-
-        MPI_Bcast(array.data(), N, MPI_INT, 0, MPI_COMM_WORLD);
-        array = PSRS(array, rank);
-
-        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-startTime).count();
-        totalTime += time;
     }
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+    //MPI_Bcast(array.data(), N, MPI_INT, 0, MPI_COMM_WORLD);
+    array = PSRS(array, rank);
+
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-startTime).count();
 
 
     if(rank == 0)
     {
-        std::cout<<" Vector: "<<std::endl; 
-        for(int i = 0; i < N; i++)
+        
+        for(int i = 1; i < N; i++)
         {
-            std::cout<<" " << array[i] << " -"; 
+            if(array[i-1] > array[i])
+            {
+                std::cout<<" not ordered " <<std::endl; 
+                break;
+            }
+            
         }
-        std::cout << std::endl << std::endl;
-        std::cout<<"Averege elapsed time: "<< (totalTime/100.0f) <<std::endl<<std::endl;
+        std::cout<<"Averege elapsed time: "<< (time/1000000.0f)<< " ms" <<std::endl<<std::endl;
     }
 
     //std::cout<<"Averege elapsed time: "<< (time/100) <<std::endl;
 
-    totalTime = 0;
-
-    for(int i = 0; i < 100; i++)
-    {
-        auto startTime  = std::chrono::high_resolution_clock::now();
-
-        if(rank == 0)
-        {
-            //Initialization of vector
-            std::mt19937 rng;
-            for(int i = 0; i < N; i++)
-            {
-                rng.seed(clock());
-                std::generate(mergeArray.begin(),mergeArray.end(),[&](){return rng()%RANGE;});
-            }
-        }
-        MPI_Bcast(mergeArray.data(), N, MPI_INT, 0, MPI_COMM_WORLD);
-        MergeSort(mergeArray, rank);
-
-
-        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-startTime).count();
-        totalTime += time;
-    }
 
     if(rank == 0)
     {
-        std::cout<<" Vector: "<<std::endl; 
-        for(int i = 0; i < N; i++)
+        switch (order)
         {
-            std::cout<<" " << array[i] << " -"; 
+            case 1: mergeArray = SortedVector(N);
+            break;
+            case 2: mergeArray = BackwardsVector(N);
+            break;
+            default: mergeArray = RandomVector(N,N*100);
+            break;
         }
-        std::cout << std::endl << std::endl;
-        std::cout<<"Averege elapsed time: "<< (totalTime/100.0f) <<std::endl<<std::endl;
+    }
+    //MPI_Bcast(mergeArray.data(), N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    startTime  = std::chrono::high_resolution_clock::now();
+    mergeArray = MergeSort(mergeArray, rank);
+
+
+    time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now()-startTime).count();
+
+    if(rank == 0)
+    {
+        /*
+        for(int i = 1; i < N; i++)
+        {
+            if(mergeArray[i-1] > mergeArray[i])
+            {
+                std::cout<<" not ordered " <<std::endl; 
+                break;
+            }
+            
+        }*/
+        std::cout<<"Averege elapsed time: "<< (time/1000000.0f)<< "ms" <<std::endl<<std::endl;
     }
 
-    
-    //MPI_Finalize();
+    int wait = 0;
+    MPI_Bcast(&wait, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+    MPI_Finalize();
 
 }
 
@@ -115,16 +148,19 @@ std::vector<int> PSRS(std::vector<int> array, int rank)
 {
     int size = N/PROCESSOR;
 
-    int *toSort = new int[size]; // -> disjointed vector where the sorting will be performed
+    //int *toSort = new int[size]; // -> disjointed vector where the sorting will be performed
+    std::vector<int> toSort(size);
     // BEGIN STEP 1 //  
     //Distribute array betwen processors
-    MPI_Scatter(array.data(), size, MPI_INT, toSort, size, MPI_INT, 0, MPI_COMM_WORLD);    
-    QuickSort(toSort, 0, size - 1); //-> apply quicksort in all processors
+    MPI_Scatter(array.data(), size, MPI_INT, toSort.data(), size, MPI_INT, 0, MPI_COMM_WORLD);    
+    //QuickSort(toSort, 0, size - 1); //-> apply quicksort in all processors
+    std::sort(toSort.begin(), toSort.end());
     // END STEP 1 //
 
     // BEGIN STEP 2 //
     int processorPivots[PROCESSOR]; // -> pivots of each processor array
-    int candidatePivots[PROCESSOR*PROCESSOR]; // -> pivots of all processors
+    //int candidatePivots[PROCESSOR*PROCESSOR]; // -> pivots of all processors
+    std::vector<int> candidatePivots(PROCESSOR*PROCESSOR);
     int pivots[PROCESSOR - 1]; // -> selected pivots to implement sort
 
     // select pivots in each processor
@@ -133,13 +169,14 @@ std::vector<int> PSRS(std::vector<int> array, int rank)
         processorPivots[i] = toSort[i*size/PROCESSOR];
     }
     //rank 0 collect all the pivots into candidate pivots
-    MPI_Gather(processorPivots, PROCESSOR, MPI_INT, candidatePivots, PROCESSOR, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(processorPivots, PROCESSOR, MPI_INT, candidatePivots.data(), PROCESSOR, MPI_INT, 0, MPI_COMM_WORLD);
 
 
     if(rank == 0)
     {        
         //order candiate pivots
-        QuickSort(candidatePivots, 0, PROCESSOR*PROCESSOR - 1);
+        //QuickSort(candidatePivots, 0, PROCESSOR*PROCESSOR - 1);
+        std::sort(candidatePivots.begin(),candidatePivots.end());
         //select pivots to use
         for(int i = 0; i < PROCESSOR - 1; i++)
         {
@@ -188,12 +225,14 @@ std::vector<int> PSRS(std::vector<int> array, int rank)
     // END STEP 3 //
     // BEGIN STEP 4 //
 
-    int *sorted = new int[rankRcvLength];
+    //int *sorted = new int[rankRcvLength];
+    std::vector<int> sorted(rankRcvLength);
 
-    MPI_Alltoallv(toSort, sizes, indexes, MPI_INT, sorted, rankRcvLen, rankStartIndexes, MPI_INT, MPI_COMM_WORLD);
+    MPI_Alltoallv(toSort.data(), sizes, indexes, MPI_INT, sorted.data(), rankRcvLen, rankStartIndexes, MPI_INT, MPI_COMM_WORLD);
 
-    QuickSort(sorted, 0, rankRcvLength - 1);
-
+    //QuickSort(sorted.data(), 0, rankRcvLength - 1);
+    std::sort(sorted.begin(), sorted.end());
+    
     // END STEP 4 //
     // FINAL STEP //
 
@@ -207,62 +246,37 @@ std::vector<int> PSRS(std::vector<int> array, int rank)
         indexes[i+1] = indexes[i] + lengths[0];
     }
 
-    MPI_Gatherv(sorted, rankRcvLength, MPI_INT, array.data(), lengths, indexes, MPI_INT, 0, MPI_COMM_WORLD);
+    //MPI_Gatherv(sorted.data(), rankRcvLength, MPI_INT, array.data(), lengths, indexes, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if(rank != 0)
+    {
+        MPI_Send(sorted.data(), rankRcvLength, MPI_INT, 0, rank, MPI_COMM_WORLD);
+    }
+
+    if(rank == 0)
+    {
+        std::vector<int> result;
+	    MPI_Status status;
+        result.insert(result.end(),sorted.begin(),sorted.end());
+
+        for(int i = 0; i < PROCESSOR; i++)
+        {
+            if(i == 0) continue;
+            MPI_Probe(i, i, MPI_COMM_WORLD, &status);
+
+            int count;
+            MPI_Get_count(&status, MPI_INT, &count);
+
+            int *aux = new int[count];
+            
+		    MPI_Recv(aux, count, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            result.insert(result.end(),aux,aux+count); 
+        }
+        return result;
+    }
         
     return array;
 }
-
-
-void Swap(int* a, int* b) 
-{ 
-	int t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
-
-/* This function takes last element as pivot, places 
-the pivot element at its correct position in sorted 
-	array, and places all smaller (smaller than pivot) 
-to left of pivot and all greater elements to right 
-of pivot */
-int Partition (int *array, int low, int high) 
-{ 
-	int pivot = array[high]; // pivot 
-	int i = (low - 1); // Index of smaller element 
-
-	for (int j = low; j <= high- 1; j++) 
-	{ 
-		// If current element is smaller than or 
-		// equal to pivot 
-		if (array[j] <= pivot) 
-		{ 
-			i++; // increment index of smaller element 
-			Swap(&array[i], &array[j]); 
-		} 
-	} 
-	Swap(&array[i + 1], &array[high]); 
-	return (i + 1); 
-} 
-
-/* The main function that implements QuickSort 
-arr[] --> Array to be sorted, 
-low --> Starting index, 
-high --> Ending index */
-void QuickSort(int *array, int low, int high)
-{ 
-	if (low < high) 
-	{ 
-		/* pi is partitioning index, arr[p] is now 
-		at right place */
-		int pi = Partition(array, low, high); 
-
-		// Separately sort elements before 
-		// partition and after partition 
-		QuickSort(array, low, pi - 1); 
-		QuickSort(array, pi + 1, high); 
-	} 
-} 
-
 
 std::vector<int> MergeSort (std::vector<int> nums, int rank) 
 {
